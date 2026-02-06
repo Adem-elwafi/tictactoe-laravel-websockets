f16446b06f85da0c3e2ecf3ae1c4a1d275e2c9d0
Adem-elwafi <adem.elwafi.dev@gmail.com>
2026-02-06 18:25:27 +0100

    chore(broadcasting): authorize private game channels by room_code

--- app/Models/Game.php
+++ app/Models/Game.php
@@ -6,7 +6,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Str;
 
-class Game extends Model
+class Game extends Model //automatically  looks for games  table (plural,lowercase)
 {
     use HasFactory;
 
--- routes/channels.php
+++ routes/channels.php
@@ -1,7 +1,21 @@
 <?php
 
 use Illuminate\Support\Facades\Broadcast;
+use App\Models\Game;
 
-Broadcast::channel('App.Models.User.{id}', function ($user, $id) {
-    return (int) $user->id === (int) $id;
+/*
+|--------------------------------------------------------------------------
+| Broadcast Channels
+|--------------------------------------------------------------------------
+|
+| Here you may register all of the event broadcasting channels that your
+| application supports.
+|
+*/
+
+Broadcast::channel('game.{room_code}', function ($user = null, string $room_code) {
+    // Allow subscription if the game exists.
+    // $user is null because we are using session-based / anonymous auth.
+    return Game::where('room_code', $room_code)->exists();
+    
 });
